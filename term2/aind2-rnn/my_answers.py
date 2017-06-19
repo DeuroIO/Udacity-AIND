0d03ed11b777aedd3442e2724ec21feed06cd2ef
Grey <chen1474@purdue.edu>
2017-06-18 23:23:23 -0700

    finish the clean text

--- term2/aind2-rnn/my_answers.py
+++ term2/aind2-rnn/my_answers.py
@@ -5,6 +5,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 import keras
 
+import re
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
@@ -23,16 +24,23 @@ def window_transform_series(array,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    model.summary()
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
-
+    chars = sorted(list(set(text)))
+    for char in chars:
+        print(char)
 
     # remove as many non-english characters and character sequences as you can 
-
+    text = re.sub('([^a-zA-Z\s])+', '', text)
+    return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text,window_size,step_size):
