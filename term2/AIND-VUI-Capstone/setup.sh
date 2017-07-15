c0321b0884cff9048ad116c97d9da793ff7a1d5c
Gelei Chen <chen1474@purdue.edu>
2017-07-15 22:14:45 +0000

    finish cnn_rnn_timedistributed training

---
+++ term2/AIND-VUI-Capstone/setup.sh
@@ -0,0 +1,9 @@
+export PATH=~/anaconda3/bin:$PATH
+conda create --name aind-vui python=3.5 numpy
+source activate aind-vui
+pip install tensorflow-gpu==1.1.0
+pip install -r requirements.txt
+KERAS_BACKEND=tensorflow python -c "from keras import backend"
+sudo apt-get install libav-tools
+jupyter notebook --ip=0.0.0.0 --no-browser
+
